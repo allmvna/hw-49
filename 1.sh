83dc3b07d21af6aa898a7c9e6c381d891bd7daee
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-09-05 01:54:42 +0600

    1-ex done!

---
+++ 1.sh
@@ -0,0 +1,10 @@
+mkdir projects
+cd projects
+mkdir my_projects
+cd my_projects
+mkdir directory1 directory2 directory3
+cd directory1 && mkdir subdirectory1 && cd ..
+cd directory2 && echo "Hello from JS" > hello.txt && cd ..
+cd directory3 && mkdir subdirectory2 && cd ..
+
+

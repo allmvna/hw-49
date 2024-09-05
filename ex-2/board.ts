e167c73dd91f762b64089b85c85bf0cd98e1d804
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-09-05 14:43:56 +0600

    Optimized the output and added a function for generating a board

--- ex-2/board.ts
+++ ex-2/board.ts
@@ -1,13 +1,13 @@
-let array: string[] = [];
+function generateChessBoard() {
+    let array: string[] = [];
 
-for (let i: number = 1; i <= 8; i++) {
-    for (let j: number = 1; j <= 8; j++) {
-        if (i % 2 === j % 2) {
-            array.push('  ');
-        } else {
-            array.push('██');
+    for (let i: number = 1; i <= 8; i++) {
+        for (let j: number = 1; j <= 8; j++) {
+            array.push((i % 2 === j % 2) ? '  ' : '██');
         }
+        console.log(array.join(''));
+        array = [];
     }
-    console.log(array.join(''));
-    array = [];
 }
+
+generateChessBoard();
